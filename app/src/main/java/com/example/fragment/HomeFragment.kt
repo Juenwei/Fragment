c44188725b07bc31f48dec6d54f2c6f668ba89f5
FORGE-15 I5 <juenweichee@gmail.com>
2022-02-20 20:11:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragment/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val about = view.findViewById<Button>(R.id.aboutBtn)
+        about.setOnClickListener()
+        {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_aboutFragment)
+        }
+
+        return view
+    }
+}
